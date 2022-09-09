1fc93de7fbe592254bcd938922757b10c6f2f7d2
flitzcore <hemistergt@gmail.com>
2022-09-09 21:12:34 +0700

    1

--- Perkenalan_Ez.c
+++ Perkenalan_Ez.c
@@ -1,8 +1,44 @@
-#include <stdio.h> 
+#include <stdio.h> ////Ju5t s0me bas1c stuff, n0thing to look 'ere
+void BolehDiubah(int *key);
+/*
+#############################################
+#############################################
+##############   WELCOME TO   ###############
+###############    LockGame  ###############
+################    by Lock_RR   ###########
+#############################################
+#############################################
+*/
 
-int main() 
+int main()
 {
-  printf("VSCode is set up correctly! Yay!\nHappy Coding!\n<3 Mikkel.");
+  int key = 5;
+  BolehDiubah(&key);
+  printf("\n\n\n");
 
-return 0;
+  return 0;
+}
+/////!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!//////
+
+///     BOLEH UBAH CODE DISINI   ///
+
+void BolehDiubah(int *key)
+{
+  int opsi;
+
+  if (*key > 0)
+  {
+    *key -= 1;
+    printf("%d\n", *key);
+    BolehDiubah(key);
+  }
+}
+
+///@!@@!@@!@!@!@!@!@!@!@!@!@!@!@@!@!@!@!@!@!@///
+
+char terjemahkanASCII(int num)
+{
+  char numInChar;
+  numInChar = (char)num;
+  return numInChar;
 }
